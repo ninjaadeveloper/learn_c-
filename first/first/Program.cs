e3989a6edad832542c9ce2c29b040cd1dfb2c735
ninjaadeveloepr <chouhdryrizwan786@gmail.com>
2024-12-16 11:03:28 +0500

    firstApp

---
+++ first/first/Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Good Morning");
+        Console.WriteLine("I'm your teacher");
+        Console.WriteLine("--------------------");
+
+        string myname = "Hamza Akram";
+        //myname = "Mudassir Ali";
+       myname  = ("20");
+
+        Console.WriteLine(myname);
+
+
+    }
+}
