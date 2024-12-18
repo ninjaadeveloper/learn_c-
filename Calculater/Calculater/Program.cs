03828fcf6deed0d80b6ca206adf65f066a8b247d
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-12-18 10:56:53 +0500

    2nd

---
+++ Calculater/Calculater/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ----- Calculater ----- ");
+
+            Console.Write("Enter your number 1 :");
+            int number1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Enter your number 2 :");
+            int number2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("The Result is : ");
+            Console.Write(number1 + number2);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondApp/SecondApp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Console.WriteLine("Hello World");
+             Console.WriteLine("Shehran");
+            */
+
+            /*  Console.Write("What is your name: ");
+              String stdname =  Console.ReadLine();
+
+              Console.Write("What is your age: ");
+              String stdage = Console.ReadLine();
+            */
+
+           // Console.WriteLine(stdname);
+            // Console.WriteLine("My student name is : " + stdname); // concatetion  
+
+            // Console.WriteLine("My fav student name is {0}", stdname); // placeholder
+
+            //  Console.WriteLine("Your name is {0} and your age is {1} ",stdname,stdage);
+
+
+            string personname = "Ali";
+           Console.WriteLine(personname);
+
+           int personage = 21;
+           Console.WriteLine(personage);
+
+           bool status = true;
+           Console.WriteLine(status);
+
+           double height = 5.2;
+           Console.WriteLine(height);
+
+           const double pi = 3.14;
+           Console.WriteLine(pi);
+
+           //const double pi = 3.15;
+
+          Console.WriteLine("Your are a \n good boy.");
+
+
+
+
+
+
+            
+
+
+           Console.ReadKey();
+
+
+}
+}
+}
