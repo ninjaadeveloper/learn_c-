8dcc2503cc4f5d3472538e76542d7ca2478ad7c6
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2024-12-30 11:01:03 +0500

    loops and array

---
+++ Arrays/Arrays/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string[] student_names = { "Shehran", "Hamza", "Jazib", "Suleman" };
+
+            Console.WriteLine(student_names[0]);
+            */
+            /*
+            int[] mynames = new int[2];
+
+            mynames[0] = 25;
+            mynames[1] = 50;
+            //mynames[2] = 70;
+
+            Console.WriteLine(mynames[1]);
+            */
+            /*
+                        int[] salary = { 25000, 35000, 15000 };
+
+                        Console.WriteLine(salary[1]);
+            */
+
+
+            string[,] students =
+             {//main array
+              
+                {"Ali","Shehran" }, //0
+                //0 ,    //1
+                { "Huzaifa", "Hammmad" }, //1
+                //0 ,          //1
+                { "Suleman", "Hamza" } //2
+                //0 ,           //1
+            };
+
+            Console.WriteLine(students[2,0]);
+
+            Console.ReadKey();
+}
+}
+}
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+
+            for (int a = 1; a <= 10; a++) 
+            {
+                Console.WriteLine("Hello World :" + a);
+            }
+
+            */
+
+            /* int i = 1;
+             while (i <= 10) {
+                 Console.WriteLine("Hello World from while loop : " + i);
+                 i++;
+             }
+             */
+            /*
+            do
+            {
+                Console.WriteLine("Hello World from do-while loop : " + i);
+                i++;
+            } while (i > 10);
+            */
+            /*
+            Console.WriteLine("Enter your table number: ");
+            int table  = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(table + " x " + i + " = " + table * i);
+            }
+            */
+
+            for (int a = 1; a <= 10; a++)
+            {
+                
+                if (a == 5) {
+                    //break;
+                    //continue;
+                    goto Mylabel;
+                }
+
+                Console.WriteLine("Hello World :" + a);
+            }
+
+
+            Mylabel:
+            Console.WriteLine("This is the outside form loop");
+
+
+
+            Console.ReadKey();
+
+}
+}
+}
