a49f259a8fa896bf5821c25b3b280815e2852874
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2025-01-15 10:52:09 +0500

    oop5

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        Student std1 = new Student()
+        {
+            stdName = "Ali",
+            StdEmail = "ali@gmail.com"
+        };
+
+        //std1.stdName = "Hamza";
+
+        Student std2 = new Student()
+        {
+            stdName = "Ali",
+            StdEmail = "ali@gmail.com"
+        };
+
+        Console.WriteLine(std1 == std2);
+        //Console.WriteLine(std1);
+
+    }
+
+}
+public record Student
+{
+    public required string stdName { get; init; }
+    public required string StdEmail { get; init; }
+};
---
+++ RecordTypes/RecordTypes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RecordTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Student std1 = new Student()
+            //{
+            //    stdName = "Hamza",
+            //    stdEmail = "hamza@gmail.com"
+            //};
+
+            //Console.WriteLine(std1.stdName);
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(std1.stdEmail);
+
+
+            //Student std2 = new Student()
+            //{
+            //    stdName = "Hamza",
+            //    stdEmail = "hamza@gmail.com"
+            //};
+
+            //Console.WriteLine(std2.stdName);
+            //Console.WriteLine("--------------");
+            //Console.WriteLine(std2.stdEmail);
+
+
+            //Console.WriteLine(std1 == std2);
+
+            int[] marks = new int[2];
+
+            //try
+            //{
+            //    marks[0] = 85;
+            //    marks[1] = 99;
+            //    marks[2] = 100;
+            //    marks[3] = 35;
+            //}
+            //catch(Exception e) {
+            //    Console.WriteLine("Error ye ha {0}",e.Message);
+            //}
+
+
+            /*Console.WriteLine("Enter your number 1");
+              int num1 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Enter your number 2");
+              int num2 = int.Parse(Console.ReadLine());
+
+              try
+              {
+                  Console.WriteLine(num1 % num2);
+              }
+              catch (Exception ex) {
+                  Console.WriteLine(ex.Message);
+              }finally{
+                Console.WriteLine("har haal meen chaly ga")
+            }
+            */
+
+            for (int i = 5; i > 0; i++) {
+                    Console.WriteLine(i);
+            }
+
+  Console.ReadKey();
+
+
+
+}
+}
+}
