eca2655fd9326733b00b1bcf92a5325bf0b8f062
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2025-01-20 10:52:06 +0500

    delegates

---
+++ test/test/Program.cs
@@ -0,0 +1,35 @@
+﻿public delegate int Calculater(int method1, int method2);
+internal class Program
+{  
+    //method 1
+    static int Add(int num1, int num2)
+    {
+
+        return (num1 + num2);
+    }
+
+    //method 2
+    static int Sub(int num1, int num2)
+    {
+
+        return (num1 - num2);
+    }
+
+    private static void Main(string[] args)
+    {
+        int value1 = 5; //field 1
+        int value2 = 10;  //field 2
+
+        Calculater newCalculater = new Calculater(Add);
+        Calculater newCalculater1 = new Calculater(Sub);
+
+        Console.WriteLine(value1 + " + " + value2 + " = " +
+         newCalculater(value1, value2));
+
+        Console.WriteLine("----------------------");
+
+        Console.WriteLine(value2 + " - " + value1 + " = " +
+         newCalculater1(value2, value1));
+
+    }
+}
---
+++ testDeligates/testDeligates/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testDeligates
+{
+    public delegate int Calculater(int method1, int method2);
+    internal class Program
+    {
+        
+
+        //method 1
+        public int Add(int num1, int num2)
+        {
+
+            return (num1 + num2);
+        }
+
+        //method 2
+        public int Sub(int num1, int num2)
+        {
+
+            return (num1 - num2);
+        }
+
+        static void Main(string[] args)
+        {
+            int value1 = 5; //field 1
+            int value2 = 10;  //field 2
+
+            Calculater newCalculater = new Calculater(Add);
+            Console.WriteLine(value1 + " + " + value2 + " = " +
+            newCalculater(value1,value2));
+        }
+    }
+}
+       
+
+       
+
+
+
+    }
+}
