937d4187bf6357022dd32c6a7b4405e869ef3a11
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2025-01-24 11:01:16 +0500

    generics

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericClass<string> myGenerics = new GenericClass<string>("Hamza");
+            myGenerics.Display();
+            Console.WriteLine("------------------------");
+
+            GenericClass<int> myGenerics2 = new GenericClass<int>(20);
+            myGenerics2.Display();
+
+            Console.ReadKey();
+
+
+
+        }
+
+
+
+        public class GenericClass<T> {
+            private  T data;
+            public GenericClass(T Value) {
+              this.data = Value;
+            }
+        public void Display() {
+            Console.WriteLine("Value is {0}",data);
+        }
+        
+        }
+    }
+}
