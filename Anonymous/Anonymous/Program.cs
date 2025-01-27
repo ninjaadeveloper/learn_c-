556beddbe99bdf6ee67977e9aaf97bebdca9a600
ninjaadeveloper <chouhdryrizwan786@gmail.com>
2025-01-27 10:57:11 +0500

    iterators and advanceC#

---
+++ Anonymous/Anonymous/Program.cs
@@ -0,0 +1,70 @@
+﻿internal class Program
+{
+    public delegate void myDelegate(int num1, int num2);
+    private static void Main(string[] args)
+    {
+        /*var student1 = new { name = "Shehran", age = 22, course = "ADSE" };
+        
+        Console.WriteLine(student1.age);
+        */
+
+        /*myDelegate md = delegate (int value1, int value2)
+         {
+               Console.WriteLine(value1 + value2);
+         };
+         md(50, 20);*/
+
+        /* string stdname = "Mudassir";
+         Console.WriteLine(stdname.myString());*/
+
+        /*Person1 myperson = new Person1();
+
+          myperson.personName();
+          Console.WriteLine("-----------------");
+          myperson.personAge();
+
+          */
+
+        string stdname = "Shehran";
+        int stdage = 20;
+        char? course = null;
+        string future = null ?? "Bohat acha ha";
+
+        Console.WriteLine("Student Name is : "+stdname);
+        Console.WriteLine("Student Age is : " + stdage);
+
+        if (course.HasValue)
+        {
+            Console.WriteLine("Student course is : " + course);
+        }
+        else {
+            Console.WriteLine("not enrolled in any course ");
+        }
+
+        Console.WriteLine("Student Future is : " + future);
+
+    }
+}
+
+    public static class myExtension {
+public static string myString(this string myname) {
+ return myname;
+}
+
+}
+
+public partial class Person1 {
+
+    public void personName() {
+        Console.WriteLine("This is from person 1");
+    }
+
+}
+
+public partial class Person1 {
+
+    public void personAge() {
+        Console.WriteLine("This is from Person 2");
+    }
+
+}
---
+++ session12/session12/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*foreach (var myValues in GetValues()) {
+                 Console.WriteLine(myValues);
+             }
+            */
+
+            foreach (var myNames in getNames()) {
+
+                Console.WriteLine(myNames);
+            }
+
+
+            Console.ReadKey();
+        }
+
+        public static IEnumerable<int> GetValues(){
+
+            yield return 1;
+            yield return 2;
+            yield return 3;
+            yield return 4;
+            yield return 5;
+
+        }
+
+        public static IEnumerable<string> getNames() {
+
+            yield return "Shehran";
+            yield return "Suleman";
+            yield return "Zaki";
+            yield return "Mudassir";
+            yield return "Jazib";
+            yield return "Aliyan";
+            yield return "Touqeer";
+        }
+
+    }
+}
